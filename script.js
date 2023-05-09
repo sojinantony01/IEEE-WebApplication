2e1784f722acaaaabd862831204991b16f487d57
User <sojin.antony@simplogics.com>
2023-05-09 19:54:42 +0530

    products created with json

--- script.js
+++ script.js
@@ -11,17 +11,41 @@ let productImage = "picsum.photos/id/237/200/300";
 const printProduct = () => {
     let productBox = document.getElementById("product_box");
     let products = []
+    const data = [{
+        name: "apple",
+        price: 2000
+    },
+    {
+        name: "apple -2",
+        price: 300
+    },
+    {
+        name: "apple -3",
+        price: 20
+    },
+    {
+        name: "apple -4",
+        price: 2
+    },
+    {
+        name: "apple -5",
+        price: 4
+    },
+    {
+        name: "apple -6",
+        price: 5
+    }]
     productBox.innerHtml = "";
-    for(let i = 0; i< 50; i++) {             
+    for(let i = 0; i< data.length; i++) {             
         products.push(`<div class="product_card_container">
                 <div class="product_card">
                     <img src="https:${productImage}" />
                     <div class="product_details">
                         <div class="product_name">
-                            ${productName}
+                            ${data[i].name}
                         </div>
                         <div class="product_price">
-                            2000$
+                            ${data[i].price}
                         </div>
                     </div>
                 </div>
